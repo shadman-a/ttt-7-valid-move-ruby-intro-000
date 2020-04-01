61495fb2130fd83ddb4aea9bb555e5016c17532a
Shadman Ahmed <shadman@outlook.com>
2020-04-01 23:44:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,25 @@
-def valid_move? (spot)
-  spot = position.to_i - 1
-  if !position_taken? (board,index) == false && spot.between? (0,8)
-return true
-  else
-return false
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
-end
 
-def position_taken? (board,index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-return false
-  else
-return true
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
   end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
 end
